bae4dcc60f2cbdd5799f7e167544d11a139dc653
boldak <boldak.andrey@gmail.com>
2024-03-19 16:25:27 +0200

    add request schema validation

--- src/javascript/service.js
+++ src/javascript/service.js
@@ -1,10 +1,45 @@
+const Ajv = require('ajv').default;
+const ajv = new Ajv({ allErrors: true });
+require('ajv-formats')(ajv);
+require('ajv-errors')(ajv /* , {singleError: true} */);
+
+
+
+const schema = {
+	type: "object",
+	required: ["text"],
+	properties:{
+		text: {
+			type: "string",
+			additionalProperties: true
+		}
+	},
+	additionalProperties: true
+}
+
+
+const validate = ajv.compile(schema)
+
+
+
+
 module.exports = worker => ({
     method: "post",
     path: "/",
     handler: async (req, res) => {
+		validate(req.body)
+    	if (validate.errors) {
     	
-    	let result = await worker.request(req.body)
-    	res.json(result)
+    		res.json({
+    			error: `Bad request body format. ${JSON.stringify(req.body, null, '')}\n${validate.errors.map( e => "On the path " + (e.instancePath || '#') + ":" + e.message ).join('')}`
+			})
+		
+		} else {	
+	    
+	    	let result = await worker.request(req.body)
+	    	res.json(result)
+	    
+	    }	
 
     }
 })
