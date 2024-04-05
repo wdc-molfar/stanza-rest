63968153f642a9fa744a74c948dc565ff9e6d1dc
bio-0 <dima.shutyak@gmail.com>
2024-04-05 19:25:08 +0300

    add summary-worker, update model-download

--- src/python/model-download.py
+++ src/python/model-download.py
@@ -1,5 +1,6 @@
 import stanza
 import os
+from transformers import pipeline
 
 def load_model():
     model_lang = os.environ.get('STANZA_SERVER_LANGUAGES')
@@ -12,3 +13,6 @@ def load_model():
                     processors='tokenize,mwt,pos,lemma,ner,sentiment,depparse,coref',
                     tokenize_no_ssplit=True,
                     use_gpu=True)
+    # summary download
+    pipe = pipeline(task="summarization", model="philschmid/bart-large-cnn-samsum", device=0) # device=-1 - CPU
+    pipe.save_pretrained('./src/python/summary-model')
---
+++ src/python/summary-worker.py
@@ -0,0 +1,56 @@
+import json
+import os
+import io
+import sys
+import warnings
+import traceback
+
+import transformers
+transformers.logging.set_verbosity_error()
+
+warnings.filterwarnings("ignore", message=r"\[W033\]", category=UserWarning)
+
+input_stream = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
+
+
+output_json = json.dumps({"status":"started"}, ensure_ascii=False).encode('utf-8')
+sys.stdout.buffer.write(output_json)
+print()        
+
+
+def main(input_json):
+    text = input_json['text']
+    output_json = input_json.copy()
+    summarizer = transformers.pipeline(
+        task="summarization",
+        model='./src/python/summary-model',
+        device=0) # device=0 GPU - device=-1 GPU
+
+    text = summarizer(text, max_length=200, min_length=30, do_sample=False)[0]
+    output_json = {"request": output_json, "response": text}
+    return output_json
+
+
+if __name__ == '__main__':
+
+    input_json = None
+    for line in input_stream:
+
+        # read json from stdin
+        input_json = json.loads(line)
+
+        try:
+
+            output = main(input_json)
+        except BaseException as ex:
+            ex_type, ex_value, ex_traceback = sys.exc_info()
+
+            output = {"error": ''}
+            output['error'] += "Exception type : %s; \n" % ex_type.name
+            output['error'] += "Exception message : %s\n" % ex_value
+            output['error'] += "Exception traceback : %s\n" % "".join(
+                traceback.TracebackException.from_exception(ex).format())
+
+        output_json = json.dumps(output, ensure_ascii=False).encode('utf-8')
+        sys.stdout.buffer.write(output_json)
+        print()
